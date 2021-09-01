2770dc9113e4935c45af1c36b8814d682681519d
Oleh Fedorenko <ofedoren@redhat.com>
2021-09-01 14:35:19 +0000

    Add main.pp

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # puppet_host_reports
 Basic manifests to test host reports plugins
+
+To test this, you can try the following:
+
+```
+ $ ssh root@machine
+ $ cd ~
+ $ git clone https://github.com/ofedoren/puppet_host_reports.git
+ $ puppet apply ~/puppet_host_reports/main.pp
+```
---
+++ main.pp
@@ -0,0 +1,23 @@
+file { '/tmp/xyz.txt':
+  ensure  => present,
+  mode    => 0666,
+  content => "Here is my Public IP Address: ${ipaddress_eth0}.\n",
+}
+
+package { 'vim':
+  ensure => installed,
+}
+
+package { 'apache2':
+  ensure => installed,
+}
+
+service { 'apache2':
+  ensure => running,
+}
+
+file { '/var/www/html/info.php':
+  ensure => file,
+  content => '<?php  phpinfo(); ?>',
+  require => Package['apache2'],
+}
